a88108d31c7b56c5806fed134b16e9808803d96c
Paolo Morandotti <50804715+pmoitaly@users.noreply.github.com>
2024-11-15 19:11:23 +0100

    Update plBindAPI.ClassFactory.pas
    In plBindAPI.ClassFactory.pas:
    New:
    - Documentation in XML Document
    - procedure AddClassToDeferredList implemented
    - procedure GetNewInstance: new instance of a class can be bound automatically
    - procedure RegisterClass raises an exception on duplicate registration
    Change:
    - constructor Create: new optional parameter AAutoBind
    - function RegisteredClass now returns a copy of FClassList

--- Src/plBindAPI.ClassFactory.pas
+++ Src/plBindAPI.ClassFactory.pas
@@ -32,48 +32,209 @@ uses
   plBindAPI.Types;
 
 type
-  { This record stores the class information }
+  /// <summary>
+  /// Represents the metadata for a registered class, including binding and creation parameters.
+  /// </summary>
   TPlClassData = record
   strict private
-    FRegisteredClass: TClass;
+    /// <summary>
+    /// Indicates whether the class is set for automatic binding.
+    /// </summary>
+    FAutoBind: Boolean;
+
+    /// <summary>
+    /// Stores the parameters required to create an instance of the class.
+    /// </summary>
     FCreateParams: TPlCreateParams;
+
+    /// <summary>
+    /// Holds the reference to the registered class type.
+    /// </summary>
+    FRegisteredClass: TClass;
+
   public
+    /// <summary>
+    /// Indicates if the class is automatically bound to data sources.
+    /// </summary>
+    property AutoBind: Boolean read FAutoBind;
+
+    /// <summary>
+    /// Provides the registered class type.
+    /// </summary>
     property RegisteredClass: TClass read FRegisteredClass;
+
+    /// <summary>
+    /// Returns the parameters used to create an instance of the registered class.
+    /// </summary>
     property CreateParams: TPlCreateParams read FCreateParams;
-    constructor Create(AClass: TClass; const AParamsSet: TPlCreateParams);
+
+    /// <summary>
+    /// Initializes a new instance of the <c>TPlClassData</c> record.
+    /// </summary>
+    /// <param name="AClass">The class type to be registered.</param>
+    /// <param name="AParamsSet">The creation parameters for the class.</param>
+    /// <param name="AAutoBind">Specifies whether the class should be automatically bound. Default is <c>False</c>.</param>
+    constructor Create(AClass: TClass; const AParamsSet: TPlCreateParams;
+      AAutoBind: Boolean = False);
   end;
 
-  TPlClassList = class(TDictionary<string, TplClassData>);
+  TPlClassList = class(TDictionary<string, TPlClassData>);
   TPlInstanceList = class(TObjectDictionary<string, TObject>);
 
+type
+  /// <summary>
+  /// Manages a set of classes for binding, including their registration,
+  /// instantiation, and lifecycle.
+  /// </summary>
   TPlClassManager = class
   private
+    /// <summary>
+    /// List of registered classes and their associated metadata.
+    /// </summary>
     class var FClassList: TPlClassList;
+
+    /// <summary>
+    /// List of instantiated objects managed by the class manager.
+    /// </summary>
     class var FInstanceList: TPlInstanceList;
+
+    /// <summary>
+    /// Creates and returns a new instance of a registered class using its metadata.
+    /// </summary>
+    /// <param name="AClassData">Metadata containing information about the class to instantiate.</param>
+    /// <returns>A new instance of the specified class.</returns>
     class function CreateInstance(AClassData: TPlClassData): TObject;
+
+    /// <summary>
+    /// Creates and returns a new instance of a class based on its name.
+    /// </summary>
+    /// <param name="AClassName">The name of the class to instantiate.</param>
+    /// <returns>A new instance of the specified class.</returns>
     class function GetNewInstance(const AClassName: string): TObject;
+
+    /// <summary>
+    /// Retrieves the singleton instance of a class by its class' name.
+    /// </summary>
+    /// <param name="AClassName">The name of the class for which to retrieve the singleton instance.</param>
+    /// <returns>The singleton instance of the specified class.</returns>
     class function GetSingletonInstance(const AClassName: string): TObject;
+
   protected
+    /// <summary>
+    /// Initializes the <c>TPlClassManager</c> class.
+    /// </summary>
     class constructor Create;
+
+    /// <summary>
+    /// Finalizes the <c>TPlClassManager</c> class and releases resources.
+    /// </summary>
     class destructor Destroy;
-    class procedure AddClassToBindingList(AKey: string; AClassData: TPlClassData;
-      AnOptionsSet: TPlBindOptionsSet);
-    class procedure AddClassToDeferredList(AClass: TClass); virtual;
+
+    /// <summary>
+    /// Adds a class to the binding list with the specified key and options.
+    /// </summary>
+    /// <param name="AKey">The unique key for the class in the binding list.</param>
+    /// <param name="AClassData">Metadata for the class to be added.</param>
+    /// <param name="AnOptionsSet">Set of options defining the binding behavior.</param>
+    class procedure AddClassToBindingList(AKey: string;
+      AClassData: TPlClassData; AnOptionsSet: TPlBindOptionsSet);
+
+    /// <summary>
+    /// Adds a class to the deferred instantiation list.
+    /// </summary>
+    /// <param name="AClass">The class type to defer instantiation for.</param>
+    /// <param name="AClassData">Metadata for the deferred class.</param>
+    class procedure AddClassToDeferredList(AClass: TClass;
+      AClassData: TPlClassData); virtual;
+
   public
-    { Public declarations }
+    /// <summary>
+    /// Remove all registered classes and instances.
+    /// </summary>
+    class procedure Clear;
+
+    /// <summary>
+    /// Retrieves an instance of a class by its name. Returns either a singleton or a new instance based on the registration.
+    /// </summary>
+    /// <param name="AClassName">The name of the class to retrieve.</param>
+    /// <returns>An instance of the specified class.</returns>
     class function GetInstance(const AClassName: string): TObject;
-    class function Instances: TplInstanceList;
+
+    /// <summary>
+    /// Returns the list of currently managed instances.
+    /// </summary>
+    /// <returns>A list of managed instances. At present, this function is
+    /// used for monitoring purposes, but could be removed in future.</returns>
+    class function Instances: TPlInstanceList;
+
+    /// <summary>
+    /// Checks if a class with the specified name is registered.
+    /// </summary>
+    /// <param name="AClassName">The name of the class to check.</param>
+    /// <returns><c>True</c> if the class is registered; otherwise, <c>False</c>.</returns>
     class function IsRegistered(const AClassName: string): Boolean;
+
+    /// <summary>
+    /// Determines if a class is registered as a singleton by its name.
+    /// </summary>
+    /// <param name="AClassName">The name of the class to check.</param>
+    /// <returns><c>True</c> if the class is a singleton; otherwise, <c>False</c>.</returns>
     class function IsSingleton(const AClassName: string): Boolean; overload;
+
+    /// <summary>
+    /// Determines if an instance is a singleton.
+    /// </summary>
+    /// <param name="AnInstance">The instance to check.</param>
+    /// <returns><c>True</c> if the instance is a singleton; otherwise, <c>False</c>.</returns>
     class function IsSingleton(const AnInstance: TObject): Boolean; overload;
+
+    /// <summary>
+    /// Registers a class with options to determine its singleton and deferred status.
+    /// </summary>
+    /// <param name="AClass">The class to register.</param>
+    /// <param name="AsSingleton">Specifies if the class should be a singleton.</param>
+    /// <param name="AsDeferred">Specifies if instantiation should be deferred. Default is <c>False</c>.</param>
+    [deprecated('Use the RegisterClass method with TPlBindOptionsSet instead.')]
     class procedure RegisterClass(AClass: TClass; AsSingleton: Boolean;
-      AsDeferred: Boolean = false); overload; deprecated;
+      AsDeferred: Boolean = False); overload;
+
+    /// <summary>
+    /// Registers a class with specified binding options.
+    /// </summary>
+    /// <param name="AClass">The class to register.</param>
+    /// <param name="AnOptionsSet">Set of binding options for the class.</param>
     class procedure RegisterClass(AClass: TClass;
       const AnOptionsSet: TPlBindOptionsSet); overload;
-    class procedure RegisterClass(AClass: TClass; const AnOptionsSet:
-        tplBindOptionsSet; const AParamsSet: TPlCreateParams); overload;
+
+    /// <summary>
+    /// Registers a class with binding options and creation parameters.
+    /// </summary>
+    /// <param name="AClass">The class to register.</param>
+    /// <param name="AnOptionsSet">Set of binding options for the class.</param>
+    /// <param name="AParamsSet">Parameters for creating an instance of the class.</param>
+    class procedure RegisterClass(AClass: TClass;
+      const AnOptionsSet: TPlBindOptionsSet;
+      const AParamsSet: TPlCreateParams); overload;
+
+    /// <summary>
+    /// Returns a copy of the list of all registered classes.
+    /// </summary>
+    /// <returns>A list of registered classes and their metadata. At present,
+    /// this function used for monitoring purposes, but could be removed in future.</returns>
     class function RegisteredClasses: TPlClassList;
-    class procedure RegisterInstance(AnObject: TObject); deprecated;
+
+    /// <summary>
+    /// Registers an instance for binding. This method is deprecated.
+    /// </summary>
+    /// <param name="AnObject">The instance to register.</param>
+    [deprecated
+      ('Instance registration is handled automatically in newer versions.')]
+    class procedure RegisterInstance(AnObject: TObject);
+
+    /// <summary>
+    /// Unregisters an instance from binding.
+    /// </summary>
+    /// <param name="AnObject">The instance to unregister.</param>
     class procedure UnregisterInstance(AnObject: TObject);
   end;
 
@@ -81,18 +242,19 @@ implementation
 
 uses
   plBindAPI.BindManagement,
-  plBindAPI.DeferredBinding, plBindAPI.RTTIUtils;
+  plBindAPI.DeferredBinding,
+  plBindAPI.RTTIUtils;
 
 resourcestring
   StrNotRegistered = ' not registered.';
   StrMethodCreateNotFound = 'Method ''Create'' not found';
 
-{ TPlClassManager }
+  { TPlClassManager }
 
 class constructor TPlClassManager.Create;
 begin
-  FClassList := TplClassList.Create;
-  FInstanceList := TplInstanceList.Create([doOwnsValues]);
+  FClassList := TPlClassList.Create;
+  FInstanceList := TPlInstanceList.Create([doOwnsValues]);
 end;
 
 class destructor TPlClassManager.Destroy;
@@ -102,20 +264,34 @@ begin
 end;
 
 class procedure TPlClassManager.AddClassToBindingList(AKey: string;
-  AClassData: TplClassData; AnOptionsSet: tplBindOptionsSet);
+  AClassData: TPlClassData; AnOptionsSet: TPlBindOptionsSet);
 begin
   FClassList.Add(AKey, AClassData);
   if Singleton in AnOptionsSet then
     FInstanceList.Add(AKey, nil);
 end;
 
-class procedure TPlClassManager.AddClassToDeferredList(AClass: TClass);
+class procedure TPlClassManager.AddClassToDeferredList(AClass: TClass;
+  AClassData: TPlClassData);
+var
+  AnObject: TObject;
 begin
-{ TODO -oPMo -cRefactoring : Why is this method not yet implemented? }
-  // TplDeferredBinding.Add(AClass);
+  AnObject := CreateInstance(AClassData);
+  TplDeferredBinding.Add(AnObject);
 end;
 
-class function TPlClassManager.CreateInstance(AClassData: TplClassData)
+class procedure TPlClassManager.Clear;
+var
+  instance: TObject;
+begin
+  FClassList.Clear;
+  for instance in FInstanceList.Values do
+    TPlBindManager.Unbind(instance);
+  FInstanceList.Clear;
+  TplDeferredBinding.Clear;
+end;
+
+class function TPlClassManager.CreateInstance(AClassData: TPlClassData)
   : TObject;
 var
   methodFound: Boolean;
@@ -129,7 +305,8 @@ begin
   for rMethod in rType.GetMethods('Create') do
     begin
       rParams := rMethod.GetParameters;
-      methodFound := TPlRTTIUtils.SameSignature(rParams, AClassData.CreateParams);
+      methodFound := TplRTTIUtils.SameSignature(rParams,
+        AClassData.CreateParams);
       if methodFound then
         begin
           rValue := rMethod.Invoke(rType.AsInstance.MetaclassType,
@@ -161,11 +338,14 @@ end;
 class function TPlClassManager.GetNewInstance(const AClassName: string)
   : TObject;
 var
-  classData: TplClassData;
+  classData: TPlClassData;
 begin
   FClassList.TryGetValue(AClassName, classData);
   Result := CreateInstance(classData);
-// HP:  TplBindManager.Bind(Result);
+  { DONE 3 -oPMo -cRefactoring : Should the new instance bound automatically?
+    like: if classData.AutoBind then TplBindManager.Bind(Result); }
+  if classData.AutoBind then
+    TplBindManager.Bind(Result);
 end;
 
 class function TPlClassManager.GetSingletonInstance(const AClassName
@@ -181,7 +361,7 @@ begin
   end;
 end;
 
-class function TPlClassManager.Instances: TplInstanceList;
+class function TPlClassManager.Instances: TPlInstanceList;
 begin
   Result := FInstanceList;
 end;
@@ -202,9 +382,9 @@ begin
 end;
 
 class procedure TPlClassManager.RegisterClass(AClass: TClass;
-  AsSingleton: Boolean; AsDeferred: Boolean = false);
+  AsSingleton: Boolean; AsDeferred: Boolean = False);
 var
-  options: tplBindOptionsSet;
+  options: TPlBindOptionsSet;
 begin
   if AsSingleton then
     options := [Singleton];
@@ -214,30 +394,31 @@ begin
 end;
 
 class procedure TPlClassManager.RegisterClass(AClass: TClass;
-  const AnOptionsSet: tplBindOptionsSet);
+  const AnOptionsSet: TPlBindOptionsSet);
 begin
   RegisterClass(AClass, AnOptionsSet, []);
 end;
 
 class procedure TPlClassManager.RegisterClass(AClass: TClass;
-  const AnOptionsSet: tplBindOptionsSet; const AParamsSet: TPlCreateParams);
+  const AnOptionsSet: TPlBindOptionsSet; const AParamsSet: TPlCreateParams);
 var
   classData: TPlClassData;
   key: string;
 begin
-  classData.Create(AClass, AParamsSet);
   key := AClass.ClassName;
-  if not FClassList.ContainsKey(key) then
-    AddClassToBindingList(key, classData, AnOptionsSet);
+  if FClassList.ContainsKey(key) then
+    raise EplBindApiException.CreateFmt('%s is already registered.', [key]);
 
+  classData.Create(AClass, AParamsSet, (AutoBind in AnOptionsSet));
+  AddClassToBindingList(key, classData, AnOptionsSet);
   if Deferred in AnOptionsSet then
-    AddClassToDeferredList(AClass);
-  TPlDeferredBinding.TestDeferred;
+    AddClassToDeferredList(AClass, classData);
+  TplDeferredBinding.ProcessDeferred;
 end;
 
-class function TPlClassManager.RegisteredClasses: TplClassList;
+class function TPlClassManager.RegisteredClasses: TPlClassList;
 begin
-  Result := FClassList;
+  Result := TPlClassList.Create(FClassList);
 end;
 
 class procedure TPlClassManager.RegisterInstance(AnObject: TObject);
@@ -246,7 +427,7 @@ var
 begin
   key := AnObject.ClassName;
   if not FInstanceList.ContainsKey(key) then
-    FInstanceList.Add(key, AnObject); // CreateInstance(AClass));
+    FInstanceList.Add(key, AnObject);
 end;
 
 class procedure TPlClassManager.UnregisterInstance(AnObject: TObject);
@@ -254,21 +435,23 @@ var
   key: string;
 begin
   key := AnObject.ClassName;
-  if not FInstanceList.ContainsKey(key) then
+  if FInstanceList.ContainsKey(key) then
     begin
-      // Free the instance: FInstanceList.Get
+      // This step frees the instance
+      { TODO 5 -oPMo -cDebugging : Should we unbind the instance? }
       FInstanceList.Remove(key);
     end;
 end;
 
 {$REGION 'TPlClassData'}
 
-constructor TPlClassData.Create(AClass: TClass; const AParamsSet:
-    TPlCreateParams);
+constructor TPlClassData.Create(AClass: TClass;
+  const AParamsSet: TPlCreateParams; AAutoBind: Boolean = False);
 var
   i: Integer;
 begin
   FRegisteredClass := AClass;
+  FAutoBind := AAutoBind;
   setLength(FCreateParams, Length(AParamsSet));
   for i := Low(AParamsSet) to High(AParamsSet) do
     FCreateParams[i] := AParamsSet[i];
