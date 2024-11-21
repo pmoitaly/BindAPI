0809417ba6c047817f27a780b3e30cbf9ff9274f
Paolo Morandotti <50804715+pmoitaly@users.noreply.github.com>
2024-11-21 09:32:51 +0100

    Packages updates
    Packages updated.

---
+++ Packages/bindapi.pas
@@ -0,0 +1,17 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit BindAPI;
+
+{$warn 5023 off : no warning about unused units}
+interface
+
+uses
+  plBindAPI.Attributes, plBindAPI.AutoBinder, plBindAPI.BinderElement, 
+  plBindAPI.BindManagement, plBindAPI.ClassFactory, plBindAPI.CoreBinder, 
+  plBindAPI.QueryPath, plBindAPI.Types;
+
+implementation
+
+end.
